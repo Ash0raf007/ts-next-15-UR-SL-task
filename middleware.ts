8d0c0540efe5801c80233e99456cb5bf316dfe29
Ahmed ibrahim elsaeed hafez <ahmedibrahim151188@gmail.com>
2024-12-14 15:19:44 +0200

    Update middleware.ts
    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,19 +7,26 @@ export default function middleware(req: NextRequest) {
   console.log(isAuthorized)
   const url = req.url;
 
-  if (!isAuthorized && url.includes("/all-products")) {
-    return NextResponse.redirect(new URL("/auth/login", req.url));
-  }
-
   const requestHeaders = new Headers(req.headers);
 
-  
   // Add new request headers only if authorized
   if (isAuthorized) {
     requestHeaders.set("Authorization", `Bearer ${isAuthorized}`);
   }
 
-  if (isAuthorized && url.includes("/auth/login")) {
-    return NextResponse.redirect(new URL("/all-products", req.url));
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+
+  if (!isAuthorized && url.includes("/all-products")) {
+    return response.redirect(new URL("/auth/login", req.url));
   }
+
+  if (isAuthorized && url.includes("/auth/login")) {
+    return response.redirect(new URL("/all-products", req.url));
+  }
+
+  return response
 }
